fd0435ab49e05f1c57d8e207b5360d340a2ab8f5
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-01-29 20:30:08 -0500

    build(api): added base route

--- src/app/api/submissions/[submissionId]/route.ts
+++ src/app/api/submissions/[submissionId]/route.ts
@@ -3,9 +3,11 @@ import { NextRequest, NextResponse } from "next/server";
 import getDbClient from "@/lib/db/db-client";
 import { handleApiResponse } from "@/lib/api/utils";
 
-export async function POST(req: NextRequest) {
-    const reqBody = await req.json();
-
+export async function GET(
+    req: NextRequest,
+    context: { params: { submissionId: string } }
+) {
+    const submissionId = context.params.submissionId;
     const dbClient = getDbClient();
 
     return NextResponse.json(handleApiResponse({}));
