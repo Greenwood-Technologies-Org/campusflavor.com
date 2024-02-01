d5a54fb5e924c3d06f8b5acf73531083c9ff561c
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-02-01 10:29:00 -0500

    build(ui): added navbar and skeleton

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+          "primary-500": "#1E1E1E",
+          "secondary-500": "#FEFEFE",
+      }
     },
   },
   plugins: [],
