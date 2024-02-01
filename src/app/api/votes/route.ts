2d8377400645dba1d3a04c511dc153ccd65d3555
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-02-01 10:54:18 -0500

    build(api): added api template functions

---
+++ src/app/api/votes/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+    return NextResponse.json({
+    });
+}
+
+export async function POST(req: Request) {
+    let body: Object | undefined;
+
+    try {
+        body = await req.json();
+    } catch (e: unknown) {
+        body = {};
+    }
+
+    return NextResponse.json({
+        body: body,
+    });
+}
