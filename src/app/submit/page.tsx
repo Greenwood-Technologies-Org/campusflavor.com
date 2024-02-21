4397b1647600d0babc8088b70a2f5d9af6fac7e2
Roshan Kern <roshan.kern@ucdenver.edu>
2024-02-21 11:14:10 -0500

    add submit page

---
+++ src/app/submit/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SubmitPage = () => {
+    return (
+        <div>
+            <h1>Submit a Design</h1>
+            {/* Add your form or other components here */}
+        </div>
+    );
+};
+
+
+
+
+export default async function Page() {
+    return <SubmitPage />;
+}
