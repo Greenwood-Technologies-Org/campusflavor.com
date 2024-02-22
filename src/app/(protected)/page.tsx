1e782d5d8ecc7ed931fec7e28c24be7f8cdfe14f
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-02-22 13:21:07 -0500

    build(auth): integrated supabase auth

---
+++ src/app/(protected)/page.tsx
@@ -0,0 +1,13 @@
+import { createClient } from "@/lib/db/server";
+import { redirect } from "next/navigation";
+
+export default async function PrivatePage() {
+    const supabase = createClient();
+
+    const { data, error } = await supabase.auth.getUser();
+    if (error || !data?.user) {
+        redirect("/");
+    }
+
+    return <p>Hello {data.user.email}</p>;
+}
