39825e30c646d35d6beb4ad197f5bdbf338d6ffb
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-02-26 16:52:04 -0500

    build(ui): added first sign in message after verify

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,9 +1,21 @@
+"use client";
+
 import { SignInForm } from "@/components/forms/signin-form";
+import { useSearchParams } from "next/navigation";
 
 export default function LoginPage() {
+    const params = useSearchParams();
+    const fsi = params.get("fsi");
+
     return (
         <div className="w-fit h-fit flex flex-col items-center gap-4">
             <h1 className="text-xl font-bold">Sign In</h1>
+            {fsi && (
+                <div className="flex flex-col text-center">
+                    <h2>Email Verified Succesfully.</h2>
+                    <h2>Please Sign In.</h2>
+                </div>
+            )}
             <SignInForm />
         </div>
     );
