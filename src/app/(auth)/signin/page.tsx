771642bb04748ac9357053c5178343b59687ec90
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-02-26 18:04:58 -0500

    build(ui): added Suspense to pages using search parameters

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -9,17 +9,17 @@ export default function LoginPage() {
     const fsi = params.get("fsi");
 
     return (
-        <div className="w-fit h-fit flex flex-col items-center gap-4">
-            <h1 className="text-xl font-bold">Sign In</h1>
-            <Suspense>
+        <Suspense>
+            <div className="w-fit h-fit flex flex-col items-center gap-4">
+                <h1 className="text-xl font-bold">Sign In</h1>
                 {fsi && (
                     <div className="flex flex-col text-center">
                         <h2>Email Verified Succesfully.</h2>
                         <h2>Please Sign In.</h2>
                     </div>
                 )}
-            </Suspense>
-            <SignInForm />
-        </div>
+                <SignInForm />
+            </div>
+        </Suspense>
     );
 }
--- src/app/(auth)/signup/verify-email/page.tsx
+++ src/app/(auth)/signup/verify-email/page.tsx
@@ -8,22 +8,22 @@ export default function VerifyEmailPage() {
     const email = params.get("email");
 
     return (
-        <div className="w-fit h-fit flex flex-col items-center gap-4">
-            <h1 className="text-xl font-bold">
-                Please verify your email address
-            </h1>
-            <div>
-                <Suspense>
+        <Suspense>
+            <div className="w-fit h-fit flex flex-col items-center gap-4">
+                <h1 className="text-xl font-bold">
+                    Please verify your email address
+                </h1>
+                <div>
                     <h2 className="text-lg">
                         We sent an email to{" "}
                         <span className="text-blue-700">{email}</span>, with a
                         verification link.
                     </h2>
-                </Suspense>
-                <h2 className="text-lg">
-                    Please click it to verify your email address.
-                </h2>
+                    <h2 className="text-lg">
+                        Please click it to verify your email address.
+                    </h2>
+                </div>
             </div>
-        </div>
+        </Suspense>
     );
 }
