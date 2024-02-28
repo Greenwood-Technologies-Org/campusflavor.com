6886c751cfbb1df959b124a2f20ec238d32aa9a4
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-02-28 15:09:28 -0500

    build(ui): added better styling

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
 import { Suspense } from "react";
 
 export default function AuthLayout({
@@ -6,8 +8,22 @@ export default function AuthLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <main className="w-full h-screen flex flex-col items-center justify-center">
-            <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+        <main className="w-full h-screen flex flex-col items-center justify-start">
+            <header className="absolute w-full h-fit p-4">
+                <Link href="/competitions">
+                    <div className="flex items-center">
+                        <Image
+                            src="/logos/logo_main_no_bg.svg"
+                            alt="Campus Flavor Logo"
+                            height={60}
+                            width={150}
+                        />
+                    </div>
+                </Link>
+            </header>
+            <div className="w-full h-full flex flex-col items-center justify-center">
+                <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+            </div>
         </main>
     );
 }
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,10 +1,25 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
 import { SignUpForm } from "@/components/forms/signup-form";
+import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
+    const router = useRouter();
+
     return (
         <div className="w-fit h-fit flex flex-col items-center gap-4">
-            <h1 className="text-xl font-bold">Sign Up</h1>
+            <h1 className="text-xl font-bold">Create an account</h1>
             <SignUpForm />
+            <div className="flex items-center w-full">
+                <div className="border-b-2 border-gray-400 flex-grow" />
+                <span className="px-2 py-0.5">or</span>
+                <div className="border-b-2 border-gray-400 flex-grow" />
+            </div>
+            <Button className="w-full" onClick={() => router.push("/signin")}>
+                Sign In
+                <span className="sr-only">Sign in</span>
+            </Button>
         </div>
     );
 }
