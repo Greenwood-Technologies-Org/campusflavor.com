bf9f430c4170738872606f28ad2d5220f39c1f2b
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-02-28 15:57:45 -0500

    build(api): removed usages of send-mail

--- src/app/(api)/api/route.ts
+++ src/app/(api)/api/route.ts
@@ -1,9 +1,6 @@
 import { NextResponse } from "next/server";
 import { handleApiResponse } from "@/lib/api/utils";
-import { sendEmail } from "@/lib/api/send-mail";
 
 export async function GET() {
-    await sendEmail();
-
     return NextResponse.json(handleApiResponse({ message: "email sent" }));
 }
