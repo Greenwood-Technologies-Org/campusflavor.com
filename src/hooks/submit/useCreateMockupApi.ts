fad7a3a2f8e1fcb699484d36f0ed75f861c92a24
Roshan Kern <roshan.kern@ucdenver.edu>
2024-03-01 10:08:14 -0500

    use correct mockup api code

--- src/hooks/submit/useCreateMockupApi.ts
+++ src/hooks/submit/useCreateMockupApi.ts
@@ -12,6 +12,20 @@ interface ApiError {
     message: string;
 }
 
+// NEEDS TO BE HIDDEN BEFORE PUBLISHING SITE
+const mediaModifierApiKey = "e4da0953-6fa4-4546-9e8d-df4b89723fef";
+
+const getPublicDesignImageUrl = async (
+    designImageBlob: string,
+): Promise<string> => {
+    // simulate delay while getting image url
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    // placeholder for the image we are trying to get
+    const publicDesignImageUrl = "https://lh3.googleusercontent.com/drive-viewer/AKGpihYNdMktv7UuoosUGnqatCpIWtZC3ymii1xr0c8MUOtU5uqbWLo7x_DhuBNx1_kHxSFsRgkkKOGdRa3iIEJzSmaTZJuByg=s2560";
+    return publicDesignImageUrl;
+}
+
 const useCreateMockupApi = () => {
     const [loading, setLoading] = useState(false);
     const [url, setUrl] = useState("");
@@ -25,11 +39,13 @@ const useCreateMockupApi = () => {
         setError("");
 
         try {
-            // const response = await callCreateMockupApi(designImageUrl, mockupType); // Uncomment this line to use the real API call
-            const response = await fakeCallCreateMockupApi(
-                designImageUrl,
-                mockupType
-            );
+            const publicDesignImageUrl = await getPublicDesignImageUrl(designImageUrl);
+
+            const response = await callCreateMockupApi(publicDesignImageUrl, mockupType); // Uncomment this line to use the real API call
+            // const response = await fakeCallCreateMockupApi(
+            //     publicDesignImageUrl,
+            //     mockupType
+            // );
             setUrl(response.url);
         } catch (e) {
             setError("Getting mockup failed");
@@ -51,48 +67,69 @@ const callCreateMockupApi = async (
 
     if (mockupType === "T-shirt") {
         mockupTypeData = {
-            nr: 520,
+            nr: 705,
             layer_inputs: [
                 {
-                    id: "juqu6evm8k4dtcu835p",
+                    id: '539fba27-3c25-4c33-b798-0b1628b3d6a0',
                     data: designImageUrl,
-                    checked: true,
+                    crop: { x: -50, y: -50, width: 600, height: 965.4 },
+                    checked: true
                 },
                 {
-                    id: "ea18e8f6-1e41-4a5e-bbe2-a469e2fea45d",
+                    id: '16d9837d-8463-49ba-934b-42653270afaa',
                     checked: true,
+                    color: { red: 255, green: 205, blue: 205 }
                 },
-            ],
+                {
+                    id: '3c7aee09-37b7-42fe-9275-b5f0c7788dc6',
+                    checked: true,
+                    color: { red: 173, green: 216, blue: 230 }
+                }
+            ]
         };
     } else if (mockupType === "Sweater") {
         mockupTypeData = {
-            nr: 520,
+            nr: 883,
             layer_inputs: [
                 {
-                    id: "juqu6evm8k4dtcu835p",
+                    id: 'd4650742-abf2-4208-a754-c97b348b1282',
                     data: designImageUrl,
-                    checked: true,
+                    crop: { x: -50, y: -70, width: 600, height: 965.4 },
+                    checked: true
                 },
                 {
-                    id: "ea18e8f6-1e41-4a5e-bbe2-a469e2fea45d",
+                    id: '9ba9ee04-4859-4f38-8837-0786af210b6d',
                     checked: true,
+                    color: { red: 255, green: 205, blue: 205 }
                 },
-            ],
+                {
+                    id: 'd47d677f-d35f-44b9-bd2c-648a0a293259',
+                    checked: true,
+                    color: { red: 173, green: 216, blue: 230 }
+                }
+            ]
         };
     } else if (mockupType === "Hoodie") {
         mockupTypeData = {
-            nr: 520,
+            nr: 153935,
             layer_inputs: [
                 {
-                    id: "juqu6evm8k4dtcu835p",
+                    id: 'c197f8ef-ddc4-4d69-b2ce-63a58ba023e7',
                     data: designImageUrl,
-                    checked: true,
+                    crop: { x: 0, y: -66, width: 500, height: 632 },
+                    checked: true
                 },
                 {
-                    id: "ea18e8f6-1e41-4a5e-bbe2-a469e2fea45d",
+                    id: '0f4ad8a2-70cf-4381-bc0a-e5f6754d388b',
                     checked: true,
+                    color: { red: 255, green: 205, blue: 205 }
                 },
-            ],
+                {
+                    id: '904d0277-b414-4179-b4e7-4ad8eedfe53d',
+                    checked: true,
+                    color: { red: 173, green: 216, blue: 230 }
+                }
+            ]
         };
     } else {
         throw new Error("Mockup type not found");
@@ -104,7 +141,7 @@ const callCreateMockupApi = async (
         headers: {
             "Content-Type": "application/json",
             Accept: "application/json",
-            api_key: "e4da0953-6fa4-4546-9e8d-df4b89723fef",
+            api_key: mediaModifierApiKey,
         },
         data: mockupTypeData,
     };
@@ -118,7 +155,7 @@ const fakeCallCreateMockupApi = async (
     designImageUrl: string,
     mockupType: string
 ): Promise<ApiResponse> => {
-    await new Promise((resolve) => setTimeout(resolve, 5000)); // Simulate delay
+    await new Promise((resolve) => setTimeout(resolve, 3000)); // Simulate delay
 
     var mockupTypeUrl;
 
