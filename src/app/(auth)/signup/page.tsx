d4afbfc82a1137943689e419527f1867469fd545
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-03-03 13:52:34 -0500

    build(ui): slightly modified user onboarding experience

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import { SignUpForm } from "@/components/forms/signup-form";
 import { useRouter } from "next/navigation";
 
@@ -11,15 +12,16 @@ export default function LoginPage() {
         <div className="w-fit h-fit flex flex-col items-center gap-4">
             <h1 className="text-xl font-bold">Create an account</h1>
             <SignUpForm />
-            <div className="flex items-center w-full">
-                <div className="border-b-2 border-gray-400 flex-grow" />
-                <span className="px-2 py-0.5">or</span>
-                <div className="border-b-2 border-gray-400 flex-grow" />
+
+            <div className="w-full h-fit flex flex-row text-gray-500 items-center justify-center">
+                <span>
+                    Already have an account?&nbsp;
+                    <Link className="text-blue-500 underline" href="/signin">
+                        Sign in
+                    </Link>
+                    .
+                </span>
             </div>
-            <Button className="w-full" onClick={() => router.push("/signin")}>
-                Sign In
-                <span className="sr-only">Sign in</span>
-            </Button>
         </div>
     );
 }
