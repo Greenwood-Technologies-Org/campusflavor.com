2786d7c72dd0d2baf1f8822ad200307149a3510a
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-03-06 14:36:27 -0500

    fix: non react references

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -51,7 +51,7 @@ const Icons = {
             strokeWidth="0.00024000000000000003"
             {...props}
         >
-            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
             <g
                 id="SVGRepo_tracerCarrier"
                 strokeLinecap="round"
@@ -59,8 +59,8 @@ const Icons = {
             ></g>
             <g id="SVGRepo_iconCarrier">
                 <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
                     d="M15.7071 4.29289C16.0976 4.68342 16.0976 5.31658 15.7071 5.70711L9.41421 12L15.7071 18.2929C16.0976 18.6834 16.0976 19.3166 15.7071 19.7071C15.3166 20.0976 14.6834 20.0976 14.2929 19.7071L7.29289 12.7071C7.10536 12.5196 7 12.2652 7 12C7 11.7348 7.10536 11.4804 7.29289 11.2929L14.2929 4.29289C14.6834 3.90237 15.3166 3.90237 15.7071 4.29289Z"
                     fill="#000000"
                 ></path>
