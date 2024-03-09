b05289fe99b4f7947cfb0557dc32d6ca530a8b79
Roshan Kern <roshan.kern@ucdenver.edu>
2024-03-08 22:05:37 -1000

    finish footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -8,14 +8,15 @@ import {
 
 const Footer = () => {
     return (
-        <footer className="w-full bg-gray-100 py-4 flex justify-center items-center">
+        <footer className="w-full border-t-2 border-black py-4 flex justify-center items-center">
+
             <a
                 href={competitionLegalStuff}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-blue-500 hover:text-blue-800 mx-2 text-xs"
             >
-                Competition Legal Terms
+                Legal Terms
             </a>
             |
             <a
@@ -24,7 +25,7 @@ const Footer = () => {
                 rel="noopener noreferrer"
                 className="text-blue-500 hover:text-blue-800 mx-2 text-xs"
             >
-                Website Terms of Use
+                Terms of Use
             </a>
             |
             <a
@@ -33,7 +34,16 @@ const Footer = () => {
                 rel="noopener noreferrer"
                 className="text-blue-500 hover:text-blue-800 mx-2 text-xs"
             >
-                Website Privacy Policy
+                Privacy Policy
+            </a>
+            |
+            <a
+                href="mailto:contact@campusflavor.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 hover:text-blue-800 mx-2 text-xs"
+            >
+                Contact Us
             </a>
         </footer>
     );
