47c48af6c05d8a272db994b08aca1e3f933ef2ee
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-03-30 16:08:55 -0400

    build: added cease and desist

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,49 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     async redirects() {
+        // return [
+        //     {
+        //         source: "/",
+        //         destination: "/design-board",
+        //         permanent: true,
+        //     },
+        //     {
+        //         source: "/shop",
+        //         destination: "https://shop.campusflavor.com/collections/cwru",
+        //         permanent: true,
+        //     },
+        // ];
+
+        // cease and desist mode
         return [
+            { source: "/api/:path*", destination: "/cease", permanent: false },
+            { source: "/auth", destination: "/cease", permanent: false },
             {
-                source: "/",
-                destination: "/design-board",
-                permanent: true,
+                source: "/reset-password",
+                destination: "/cease",
+                permanent: false,
+            },
+            { source: "/signin", destination: "/cease", permanent: false },
+            { source: "/signout", destination: "/cease", permanent: false },
+            { source: "/signup", destination: "/cease", permanent: false },
+            { source: "/submit", destination: "/cease", permanent: false },
+            {
+                source: "/design-board",
+                destination: "/cease",
+                permanent: false,
+            },
+            { source: "/submit-info", destination: "/cease", permanent: false },
+            { source: "/about", destination: "/cease", permanent: false },
+            { source: "/legal-terms", destination: "/cease", permanent: false },
+            {
+                source: "/privacy-policy",
+                destination: "/cease",
+                permanent: false,
             },
             {
-                source: "/shop",
-                destination: "https://shop.campusflavor.com/collections/cwru",
-                permanent: true,
+                source: "/terms-of-use",
+                destination: "/cease",
+                permanent: false,
             },
         ];
     },
---
+++ src/app/(cease)/cease/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Link from "next/link";
+
+export default function CeaseAndDesist() {
+    return (
+        <div className="w-1/2 h-3/5 flex flex-col">
+            <p className="text-lg">
+                Hello,
+                <br />
+                <br />
+                We got a{" "}
+                <Link
+                    className="text-blue-500 underline"
+                    href={"/documents/cease-and-desist-letter.pdf"}
+                >
+                    cease and desist
+                </Link>{" "}
+                letter from the school.
+                <br />
+                <br />
+                We are trying to talk it out with them. The outcome is
+                uncertain.
+                <br />
+                <br />
+                We thank everyone who submitted designs, shared this with their
+                friends, or just thought this was a funny attempt at improving
+                our spirit.
+                <br />
+                <br />
+                Regards,
+                <br />
+                The Campus Flavor Team.
+            </p>
+        </div>
+    );
+}
