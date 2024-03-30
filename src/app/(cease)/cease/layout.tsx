ca8a3c840587ff457b11875ab953278ee85eb90c
Arturo Muñoz <101126374+munozarturo@users.noreply.github.com>
2024-03-30 16:39:01 -0400

    Cease and desist (#96)
    * ref: changed message on submit button in /submit to preview
    
    * build: added cease and desist
    
    * build: added responsiveness to the /cease page
    
    * build: unlinked cease and desist letter
    
    * build: unlinked cease and desist letter
    
    * build: unlinked cease and desist letter
    
    * build: updated cease page
    
    * build: adde redirect from landing to /cease

---
+++ src/app/(cease)/cease/layout.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Suspense } from "react";
+
+export default function CeaseLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <main className="w-full h-screen flex flex-col items-center justify-start">
+            <header className="absolute w-full h-fit p-4">
+                <div className="flex items-center">
+                    <Image
+                        src="/logos/logo_main_no_bg.svg"
+                        alt="Campus Flavor Logo"
+                        height={60}
+                        width={150}
+                        priority
+                    />
+                </div>
+            </header>
+            <div className="w-full h-full flex flex-col items-center justify-center">
+                <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+            </div>
+        </main>
+    );
+}
